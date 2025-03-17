41ee656dbe5288ecf84768acbec0500a723c1166
Łukasz Osiewicz <osiewicz87@gmail.com>
2025-03-17 20:05:09 +0100

    feat(logger): added counting IoU per class and mIou

--- src/counting_functions.py
+++ src/counting_functions.py
@@ -13,13 +13,49 @@ def calculate_metrics_per_class(y_true, y_pred):
         y_true_binary = (y_true == cls).astype(int)
         y_pred_binary = (y_pred == cls).astype(int)
 
+        tp = np.sum((y_true == cls) & (y_pred == cls))
+        fp = np.sum((y_true != cls) & (y_pred == cls))
+        fn = np.sum((y_true == cls) & (y_pred != cls))
+        union = tp + fp + fn
+
+
         metrics[cls.item()] = {
             'accuracy': accuracy_score(y_true_binary, y_pred_binary),
             'precision': precision_score(y_true_binary, y_pred_binary),
             'recall': recall_score(y_true_binary, y_pred_binary),
-            'f1': f1_score(y_true_binary, y_pred_binary)
+            'f1': f1_score(y_true_binary, y_pred_binary),
+            'IoU': tp/union if union != 0 else 0
         }
 
     return metrics
 
 
+def count_metrics(y_true, y_pred):
+    y_pred = torch.softmax(y_pred, dim=1).argmax(dim=1).cpu().detach().numpy()
+    y_true = y_true.cpu().detach().numpy()
+
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, average='weighted')
+    recall = recall_score(y_true, y_pred, average='weighted')
+    f1 = f1_score(y_true, y_pred, average='weighted')
+
+    # Count mIoU
+    classes = np.unique(y_true)
+    iou = []
+
+    for cls in classes:
+        tp = np.sum((y_true == cls) & (y_pred == cls))
+        fp = np.sum((y_true != cls) & (y_pred == cls))
+        fn = np.sum((y_true == cls) & (y_pred != cls))
+        union = tp + fp + fn
+        iou.append(tp/union if union != 0 else 0)
+
+    mIoU = np.mean(iou)
+
+    return {
+        'accuracy': accuracy,
+        'precision': precision,
+        'recall': recall,
+        'f1': f1,
+        'mIoU': mIoU
+    }
--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from callbacks import PrintMetricsCallback
 from dataset import ForestDataModule
 from dataset_functions import download_data, load_dataset
 from git_functions import get_git_branch, generate_short_hash
-from counting_functions import calculate_metrics_per_class
+from counting_functions import calculate_metrics_per_class, count_metrics
 from model import ResNetClassifier
 from visualization_functions import (show_n_samples, plot_metrics,
                                      get_confusion_matrix,
@@ -108,19 +108,25 @@ def main():
     preds = model.predictions
     targets = model.targets
 
-    metrics = calculate_metrics_per_class(targets, preds)
+    # Log metrics
+    metrics_per_experiment = count_metrics(targets, preds)
+    for key, value in metrics_per_experiment.items():
+        wandb.log({key: value})
 
-    # log classes which model took as input
-    accs = [metrics[key]['accuracy'] for key in metrics.keys()]
-    precs = [metrics[key]['precision'] for key in metrics.keys()]
-    recs = [metrics[key]['recall'] for key in metrics.keys()]
-    f1s = [metrics[key]['f1'] for key in metrics.keys()]
+    # Log metrics per class and classnames
+    metrics_per_class = calculate_metrics_per_class(targets, preds)
+    accs = [metrics_per_class[key]['accuracy'] for key in metrics_per_class.keys()]
+    precs = [metrics_per_class[key]['precision'] for key in metrics_per_class.keys()]
+    recs = [metrics_per_class[key]['recall'] for key in metrics_per_class.keys()]
+    f1s = [metrics_per_class[key]['f1'] for key in metrics_per_class.keys()]
+    ious = [metrics_per_class[key]['IoU'] for key in metrics_per_class.keys()]
     names_and_labels = [[key, value] for key, value in label_map.items()]
-    logged_metrics = [[name, label, acc, prec, rec, f1] for [name, label], acc, prec, rec, f1 in zip(names_and_labels, accs, precs, recs, f1s)]
+    logged_metrics = [[name, label, acc, prec, rec, f1, iou] for [name, label], acc, prec, rec, f1, iou in zip(names_and_labels, accs, precs, recs, f1s, ious)]
 
-    training_table = wandb.Table(columns=['Class name', 'Label', 'Accuracy', 'Precision', 'Recall', 'F1-score'], data=logged_metrics)
+    training_table = wandb.Table(columns=['Class name', 'Label', 'Accuracy', 'Precision', 'Recall', 'F1-score', 'IoU'], data=logged_metrics)
     wandb.log({'Classes': training_table})
 
+    # Log confusion matrix, precision-recall curve and roc-auc curve
     get_confusion_matrix(preds, targets, class_names=list(label_map.keys()))
     get_roc_auc_curve(preds, targets, class_names=list(label_map.keys()))
     get_precision_recall_curve(preds, targets, class_names=list(label_map.keys()))
