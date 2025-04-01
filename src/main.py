fix(main): Remove mixed precision and add cudnn

src/main.py
@@ -110,16 +110,15 @@ def main():
         log_model=True
     )
 
-    bf16_supported = torch.cuda.is_bf16_supported()
-    precision = "bf16-mixed" if bf16_supported else "16-mixed"
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.enabled = True
 
     trainer = Trainer(
         logger=wandb_logger,
         max_epochs=max_epochs,
         accelerator=device,
         devices=1,
-        callbacks=callbacks,
-        precision=precision
+        callbacks=callbacks
     )
 
     trainer.fit(model, datamodule)