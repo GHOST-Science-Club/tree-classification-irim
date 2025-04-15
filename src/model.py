b5c68c0118351f1395d4b737ea98acd848f8fdee
Adam Mazur <adammazur445@gmail.com>
2025-04-15 12:48:15 +0200

    fix(model): Change step_size in the LR scheduler to account for curriculum learning

--- src/model.py
+++ src/model.py
@@ -98,8 +98,8 @@ class ResNetClassifier(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.fc.parameters(), lr=self.hparams.learning_rate)
 
-        # Decay LR by a factor of 0.1 every 1 epoch
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)  # TODO: tune parameters
+        # Decay LR by a factor of 0.1 every 13 epochs
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=13, gamma=0.1)  # TODO: tune parameters
 
         return {
             'optimizer': optimizer,
